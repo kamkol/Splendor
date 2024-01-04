5dfd0c8594c3e5bb9c4eb95f25ed348952500fb6
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-01-04 16:53:42 +0100

    getters & setters

--- src/main/java/repository/Card.java
+++ src/main/java/repository/Card.java
@@ -5,5 +5,24 @@ public class Card {
 	private int cardLevel;
 	private int costCard;
 	private int cardPoints;
+	
+	public int getCardLevel() {
+		return cardLevel;
+	}
+	public void setCardLevel(int cardLevel) {
+		this.cardLevel = cardLevel;
+	}
+	public int getCostCard() {
+		return costCard;
+	}
+	public void setCostCard(int costCard) {
+		this.costCard = costCard;
+	}
+	public int getCardPoints() {
+		return cardPoints;
+	}
+	public void setCardPoints(int cardPoints) {
+		this.cardPoints = cardPoints;
+	}
 
 }
--- src/main/java/repository/Token.java
+++ src/main/java/repository/Token.java
@@ -3,6 +3,19 @@ package repository;
 public class Token {
 	
 	private int numberOfToken;
+	private int maxNumberOfToken;
 	
+	public int getNumberOfToken() {
+		return numberOfToken;
+	}
+	public void setNumberOfToken(int numberOfToken) {
+		this.numberOfToken = numberOfToken;
+	}
+	public int getMaxNumberOfToken() {
+		return maxNumberOfToken;
+	}
+	public void setMaxNumberOfToken(int maxNumberOfToken) {
+		this.maxNumberOfToken = maxNumberOfToken;
+	}
 
 }
