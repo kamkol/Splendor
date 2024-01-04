e87fe54c7e0e12a895917e4676d158fca09d6b84
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-01-04 17:28:06 +0100

    removed autowire

--- src/main/java/com/splendor/controller/GameController.java
+++ src/main/java/com/splendor/controller/GameController.java
@@ -1,6 +1,5 @@
 package com.splendor.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.splendor.service.GameService;
@@ -11,7 +10,6 @@ public class GameController {
 	
 	private final GameService gameService;
 	
-	@Autowired
 	public GameController(GameService gameService) {
 		this.gameService = gameService;
 	}
