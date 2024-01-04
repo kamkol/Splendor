d813957ce5a7fb9c7633ae6ab7711b2696335a17
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-01-04 17:16:20 +0100

    exceptions - token/aristocrat/card not found

---
+++ src/main/java/exceptions/CardNotFound.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class CardNotFound {
+	public CardNotFound(String message) {
+		super();
+	}
+	
+
+}
---
+++ src/main/java/exceptions/TokenNotFound.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class TokenNotFound {
+
+	public TokenNotFound(String message) {
+		super();
+	}
+	
+}
