969cdda676a9d15e0c33f9523be12b20179debe5
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-01-09 16:16:03 +0100

    game controller file changes

--- src/main/java/com/splendor/controller/GameController.java
+++ src/main/java/com/splendor/controller/GameController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.splendor.service.GameService;
 
-//@RestController
-//@RequestMapping
+@RestController
+@RequestMapping
 public class GameController {
 	
-//	private final GameService gameService;
-//	
-//	public GameController(GameService gameService) {
-//		this.gameService = gameService;
-//	}
+	private final GameService gameService;
+	
+	public GameController(GameService gameService) {
+		this.gameService = gameService;
+	}
 }
