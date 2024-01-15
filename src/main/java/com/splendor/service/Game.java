2e4151da14bf22c94d5658c18b3f64671cfc5e4b
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-01-15 15:47:50 +0100

    entityscan

--- src/main/java/com/splendor/service/Game.java
+++ src/main/java/com/splendor/service/Game.java
@@ -2,20 +2,23 @@ package com.splendor.service;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import com.splendor.repository.Card;
 import com.splendor.repository.Player;
 
 @Entity
+@EntityScan("com.splendor")
 public class Game {
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-//	@OneToMany
+	@OneToMany
 	private List<Player> players;
 	
-//	@OneToMany
+	@OneToMany
 	private List<Card> availableCards;
 	
 }
