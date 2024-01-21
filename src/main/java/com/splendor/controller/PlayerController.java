f14ad98be7d8d868a24ce8d0130bc4c6f8320531
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-01-21 14:39:42 +0100

    green tokens logic

--- src/main/java/com/splendor/controller/PlayerController.java
+++ src/main/java/com/splendor/controller/PlayerController.java
@@ -1,5 +1,22 @@
 package com.splendor.controller;
 
-public class PlayerController {
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.splendor.service.PlayerService;
+
+public class PlayerController {
+	
+	@Autowired
+	private PlayerService playerService;
+	
+	@PostMapping
+	public ResponseEntity<String> addGreenTokens(@PathVariable playerId, @PathVariable int tokensToAdd) {
+		playerService.addGreenTokens(playerId, tokensToAdd);
+		return ResponseEntity.ok("Green tokens added successfully")
+	}
+	
 }
