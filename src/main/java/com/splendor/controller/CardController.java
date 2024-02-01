56bde318918d6253356c6df1cba54d6dfdfbec0c
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-01 16:15:14 +0100

    controllers mapping

--- src/main/java/com/splendor/controller/CardController.java
+++ src/main/java/com/splendor/controller/CardController.java
@@ -1,7 +1,13 @@
 package com.splendor.controller;
 
+import com.splendor.repository.AristorcratCard;
+import com.splendor.repository.Card;
+import com.splendor.service.CardService;
+
 public class CardController {
 	
-	
+	private CardService cardService;
+	private Card card;
+	private AristorcratCard aristorcratCard;
 
 }
