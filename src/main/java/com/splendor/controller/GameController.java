90b8daba57c3e70ccb97703d50d554ea227702a1
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-01 15:57:51 +0100

    create new game

--- src/main/java/com/splendor/controller/GameController.java
+++ src/main/java/com/splendor/controller/GameController.java
@@ -1,14 +1,28 @@
 package com.splendor.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.splendor.repository.Game;
+import com.splendor.service.GameService;
+
 import org.springframework.ui.Model;
 
 
 @RequestMapping
 @Controller
 public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@PostMapping("/games")
+	public Game createNewGame() {
+		return gameService.createGame();
+	}
 	
 	@GetMapping("/game")
 	public String gamePage(Model model) {
