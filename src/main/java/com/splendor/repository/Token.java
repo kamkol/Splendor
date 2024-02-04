1b368fd6b3063a8a7d177e05d1f76679fe358e8e
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-04 10:28:04 +0100

    validater

--- src/main/java/com/splendor/repository/Token.java
+++ src/main/java/com/splendor/repository/Token.java
@@ -2,23 +2,9 @@ package com.splendor.repository;
 
 public class Token {
 	
-	private Long id;
-	private String type;
 	private int numberOfToken;
 	private int maxNumberOfToken;
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
 	public int getNumberOfToken() {
 		return numberOfToken;
 	}
