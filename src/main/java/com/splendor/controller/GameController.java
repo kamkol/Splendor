995dba10c29a458587ffbe05eb4ee6cccd47d8aa
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-06 15:30:22 +0100

    generate Tokens & generateTokensModel

--- src/main/java/com/splendor/controller/GameController.java
+++ src/main/java/com/splendor/controller/GameController.java
@@ -1,5 +1,8 @@
 package com.splendor.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.splendor.repository.Game;
 import com.splendor.repository.GameDetails;
 import com.splendor.service.GameService;
+
+import ch.qos.logback.core.subst.Token;
 
 import org.springframework.ui.Model;
 
@@ -32,6 +37,20 @@ public class GameController {
 	public String gamePage(Model model) {
 		model.addAttribute("message", "Splendor game!");
 		return "game";
+	}
+	
+	@GetMapping
+	public String generateTokensModel(Model model) {
+		List<Token> tokens = generateTokens();
+		model.addAttribute("tokens", tokens);
+		return "game";
+	}
+	
+	private List<Token> generateTokens() {
+		List<Token> tokens = new ArrayList<Token>();
+		tokens.add(new Token("Red", 1));
+		tokens.add(new Token("Blue", 2));
+		return tokens;
 	}
 	
 }
