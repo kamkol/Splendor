6045a451fbf58bd3111c859f2a8f033cee6f3caf
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-02-08 12:53:39 +0100

    deleting autowired

--- src/main/java/com/splendor/controller/TokenController.java
+++ src/main/java/com/splendor/controller/TokenController.java
@@ -2,7 +2,6 @@ package com.splendor.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
