8ab466326a72ee1c530d84c7714a52906ef6c187
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-07-24 19:09:08 +0200

    Card, Game, Player services

--- src/main/java/com/splendor/entity/Game.java
+++ src/main/java/com/splendor/entity/Game.java
@@ -1,7 +1,10 @@
 package com.splendor.entity;
 
+import jakarta.persistence.Entity;
 
+@Entity
 public class Game {
 
+	
 	
 }
---
+++ src/main/java/com/splendor/service/GameService.java
@@ -0,0 +1,9 @@
+package com.splendor.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+	
+
+}
