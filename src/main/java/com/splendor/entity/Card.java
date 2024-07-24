1abc145c2c4e3f26499527b37d8fc9963be8cf4e
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-07-24 18:53:32 +0200

    Card entity

--- src/main/java/com/splendor/entity/Card.java
+++ src/main/java/com/splendor/entity/Card.java
@@ -1,8 +1,37 @@
 package com.splendor.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Card {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idLong;
+	private String costOfCardString;
+	private int points;
 	
+	public Long getIdLong() {
+		return idLong;
+	}
+	public void setIdLong(Long idLong) {
+		this.idLong = idLong;
+	}
+	public String getCostOfCardString() {
+		return costOfCardString;
+	}
+	public void setCostOfCardString(String costOfCardString) {
+		this.costOfCardString = costOfCardString;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
 	
 }
--- src/main/java/com/splendor/entity/Game.java
+++ src/main/java/com/splendor/entity/Game.java
@@ -1,5 +1,7 @@
 package com.splendor.entity;
 
+
 public class Game {
 
+	
 }
