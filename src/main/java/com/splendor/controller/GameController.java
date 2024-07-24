eed85ce6de408d299f2a0397ccc73a99a59cc566
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-07-24 19:16:50 +0200

    refactoring

---
+++ src/main/java/com/splendor/controller/GameController.java
@@ -0,0 +1,14 @@
+package com.splendor.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.splendor.service.GameService;
+
+@RestController
+@RequestMapping("/splendor")
+public class GameController {
+
+	private GameService
+	
+}
--- src/main/java/com/splendor/service/GameService.java
+++ src/main/java/com/splendor/service/GameService.java
@@ -2,4 +2,6 @@ package com.splendor.service;
 
 public interface GameService {
 
+	
+	
 }
