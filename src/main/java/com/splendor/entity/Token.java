816ebce456707c139399bb728866272ff82ad2da
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-07-24 19:00:00 +0200

    Player and Token entities

--- src/main/java/com/splendor/entity/Token.java
+++ src/main/java/com/splendor/entity/Token.java
@@ -1,7 +1,15 @@
 package com.splendor.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Token {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private int numberOfToken;
 	private int maxNumberOfToken;
