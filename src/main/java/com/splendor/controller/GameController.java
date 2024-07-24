9a48b83b27e96d17fe0614ab81c7d536ac15408f
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-07-24 19:18:39 +0200

    Refactoring

--- src/main/java/com/splendor/controller/GameController.java
+++ src/main/java/com/splendor/controller/GameController.java
@@ -3,8 +3,6 @@ package com.splendor.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.splendor.service.GameRepository;
-
 @RestController
 @RequestMapping("/splendor")
 public class GameController {
