2304ff57f4844e2e479a54519344459e7e6337f5
Kamil Kołtan <40299216+kamkol@users.noreply.github.com>
2024-07-25 18:23:47 +0200

    GameService

--- src/main/java/com/splendor/repository/GameRepository.java
+++ src/main/java/com/splendor/repository/GameRepository.java
@@ -1,9 +1,7 @@
 package com.splendor.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.splendor.entity.Game;
 import com.splendor.entity.Player;
 
--- src/main/java/com/splendor/service/GameService.java
+++ src/main/java/com/splendor/service/GameService.java
@@ -1,17 +1,26 @@
 package com.splendor.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.splendor.repository.CardRepository;
 import com.splendor.repository.GameRepository;
+import com.splendor.repository.PlayerRepository;
 
 @Service
 public class GameService {
 	
-	@Autowired
 	private GameRepository gameRepository;
-
-	public void startNewGame() {
-		
+	private PlayerRepository playerRepository;
+	private CardRepository cardRepository;
+	
+	
+	public GameService(GameRepository gameRepository, PlayerRepository playerRepository,
+			CardRepository cardRepository) {
+		super();
+		this.gameRepository = gameRepository;
+		this.playerRepository = playerRepository;
+		this.cardRepository = cardRepository;
 	}
+	
+	
+	
 }
